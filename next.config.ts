0edc77d8ff0633e923a3c701a5aa47ca1404c746
sarawutms <88425757+sarawutms@users.noreply.github.com>
2026-09-25 14:29:09 +0700

    fix: turbopack config

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,7 @@ const withPWA = withPWAInit({
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {}, // Silence Turbopack warning with next-pwa
 };
 
 export default withPWA(nextConfig);
